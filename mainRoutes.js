1d58e3f1eb150e091739f9e35395ab572bd016bd
Jared <jaredping@yahoo.com>
2018-09-22 23:55:20 +0200

    Serve Landing page and About page

--- mainRoutes.js
+++ mainRoutes.js
@@ -3,7 +3,11 @@ var express = require("express");
 var mainRouter = express.Router();
 
 mainRouter.get('/', function(req, res) {
-    res.send('Hello world');
+    res.sendFile(path.join(__dirname, 'views', 'landingPage.html'));
+});
+
+mainRouter.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
 });
 
 module.exports = mainRouter;
